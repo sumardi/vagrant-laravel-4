4badc29b5aa89b0655e27f32e23f3b913f74fcfc
Sumardi Shukor <me@sumardi.net>
2014-04-13 02:53:10 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,63 @@
+# Laravel 4 with Vagrant
+Easy development environment for Laravel 4 with Vagrant. `Shell` is used for provisioning. Maintained by [Sumardi Shukor](https://about.me/sumardi).
+
+## Requirements
+* [Virtualbox 4.3.x](https://www.virtualbox.org/wiki/Downloads)
+* [Vagrant 1.5.x](https://www.vagrantup.com/downloads.html)
+
+## Setup
+
+First, clone this repository.
+```
+git clone https://github.com/sumardi/vagrant-laravel-4.git
+```
+Then, fire up Vagrant.
+```
+vagrant up
+```
+
+The first time you run vagrant it will need to fetch the virtual box image which is ~300mb so depending on your download speed this could take some time. Grab a cup of `0xC00FEE`!
+
+After the provisioning has completed, you can access your project at http://192.168.13.37 in a browser.
+
+> Note: You may have to change permissions on the public/laravel/app/storage folder to writeable under the host OS.
+
+> For example: `chmod -R 777 public/laravel/app/storage`
+
+## Installed Software
+
+* Centos 6.5 (32bit)
+* Apache 2
+* MySQL 5
+* PHP 5.4 (with mbstring, mysql, curl, gd, dom, mcrypt, imap, xdebug, pdo, pear)
+* Composer
+* PHPUnit
+* Beanstalkd
+* Supervisord
+
+## Default Credentials
+### MySQL
+* User: root
+* Password: (blank)
+* Port Forwarded : `3306` => `1337`
+
+### Apache
+* ServerName : localhost.localdomain
+* Port Forwarded : `80` => `8080`
+
+### Beanstalkd
+* Port : `11300`
+
+## Resources
+
+### Vagrant
+
+Vagrant is very [well documented](http://docs.vagrantup.com/v2/) but here are a few common commands:
+
+* `vagrant up` starts and provisions the vagrant environment
+* `vagrant suspend` suspends the machine
+* `vagrant resume` resume a suspended vagrant machine
+* `vagrant halt` stops the vagrant machine
+* `vagrant ssh` connects to machine via SSH
+* `vagrant destroy` stops and deletes all traces of the vagrant machine
+* `vagrant reload` restarts vagrant machine, loads new Vagrantfile configuration
---
+++ bootstrap.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+echo ">>> Starting Install Script"
+
+# Update
+sudo yum -y --verbose update
+sudo rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm
+sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+
+echo ">>> Installing Base"
+
+# http://bixly.com/blog/supervisord-or-how-i-learned-to-stop-worrying-and-um-use-supervisord/
+sudo groupadd supervisor
+sudo usermod -a -G supervisor vagrant
+
+# Install base items
+# sudo yum install -y --verbose vim curl wget build-essential python-software-properties python-setuptools libaio gcc gcc-c++ make automake autoconf
+
+# python stuffs
+# sudo yum install -y --verbose zlib-devel bzip2-devel openssl-devel ncurses-devel
+# echo ">>> Installing Ruby Items ... tools etc ..."
+
+# Ruby has many useful tools
+# sudo yum -y install ruby rubygems  curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
+# sudo yum -y install ruby-rdoc ruby-devel
+# sudo gem update --system
+
+# Update Again
+# sudo yum -y --verbose update
+
+echo ">>> Installing Apache, PHP 5.4 & MySQL ..."
+
+# Install all the other full stuff for web we need
+sudo yum install -y --verbose git-core php54w php54w-cli php54w-common httpd php54w-curl php54w-mbstring php54w-mysql php54w-gd php54w-dom php54w-mcrypt php54w-imap php54w-xdebug php54w-pdo php54w-pear
+sudo yum install -y --verbose mysql mysql-server
+sudo chmod -R 755 /var/lib/mysql/
+sudo service mysqld restart
+
+# make sure mysqld restarts with the server
+sudo chkconfig --add mysqld
+sudo chkconfig --level 345 mysqld on
+
+# Install PHPUnit
+command -v phpunit >/dev/null 2>&1 || {
+    sudo pear upgrade-all
+    sudo pear channel-discover pear.phpunit.de
+    sudo pear install phpunit/PHPUnit
+}
+
+# PHP Config
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php.ini
+sudo sed -i "s/#ServerName www.example.com:80/ServerName localhost.localdomain/" /etc/httpd/conf/httpd.conf
+
+echo ">>> Configured web services ... restarting httpd"
+sudo service httpd restart
+
+# make sure httpd restarts with the server
+sudo chkconfig --add httpd
+sudo chkconfig --level 345 httpd on
+
+# Beanstalkd
+command -v beanstalkd >/dev/null 2>&1 || {
+    echo ">>> Installing beanstalkd "
+    sudo yum install -y --verbose beanstalkd
+    sudo service beanstalkd start
+
+    # make sure beanstalkd restarts with the server
+    sudo chkconfig --add beanstalkd
+    sudo chkconfig --level 345 beanstalkd on
+}
+
+# Composer
+command -v /usr/local/bin/composer >/dev/null 2>&1 || {
+    echo ">>> Installing Composer"
+    curl -sS https://getcomposer.org/installer | php
+    sudo mv composer.phar /usr/local/bin/composer
+}
+
+if [ ! -d "/var/www/laravel" ]; then
+    echo ">>> Installing Laravel"
+    cd /var/www
+    /usr/local/bin/composer create-project laravel/laravel --prefer-dist
+    sudo chmod --recursive a+rw /var/www/laravel/app/storage
+    sudo cp /vagrant/conf/httpd/laravel.conf /etc/httpd/conf.d/laravel.conf
+else
+    echo ">>> Updating Laravel"
+    cd /var/www/laravel
+    /usr/local/bin/composer update
+fi
+
+echo ">>> Configured web services ... restarting httpd"
+sudo service httpd restart
+
+command -v supervisord >/dev/null 2>&1 || {
+    echo ">>> Installing supervisor"
+
+    sudo yum install -y --verbose supervisor
+    sudo mkdir /etc/supervisor.d/
+    sudo touch /etc/supervisor.d/laravel-listener.conf
+    sudo touch /etc/supervisord.conf
+    # sudo mkdir /var/log/supervisor/
+    # sudo touch /var/log/supervisor/supervisord.log
+    sudo cp /vagrant/conf/supervisord/supervisord.conf /etc/supervisord.conf
+    sudo cp /vagrant/conf/supervisord/laravel-listener.conf /etc/supervisor.d/laravel-listener.conf
+
+    # str=$'[include]\nfiles = /etc/supervisord.d/*.conf'
+    # sudo sh -c "echo '$str' >> /etc/supervisord.conf"
+    sudo supervisord -c /etc/supervisord.conf
+    # sudo supervisorctl reread
+
+    # make sure supervisord restarts with the server
+    sudo chkconfig --add supervisord
+    sudo chkconfig --level 345 supervisord on
+}
+
+echo ">>> Done!"
